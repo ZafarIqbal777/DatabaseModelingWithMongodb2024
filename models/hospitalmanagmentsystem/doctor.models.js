d66df91cf9ae2cadb619a3fa7f768214167bd4b4
ZafarIqbal777 <zafariqbal.itnets@gmail.com>
2024-03-01 14:44:07 +0500

    update in docker

--- models/hospitalmanagmentsystem/doctor.models.js
+++ models/hospitalmanagmentsystem/doctor.models.js
@@ -1,5 +1,32 @@
-import mangoose from "mangoose"
+import mongoose from "mogoose"
+import { Hospital } from "./hospital.models"
 
-const doctorSchema =  new mangoose.Schema({},{timestamps: true})
+
+
+const doctorSchema =  new mangoose.Schema({
+
+    name : {
+        type: String,
+        required : true
+    },
+    salary : {
+        type: String,
+        required : true
+    },
+    qualification : {
+        type: String,
+        required : true
+    },
+    exprienceInYears : {
+        type: Number,
+        required : true
+    },
+    worksInHospitals : [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: Hospital
+        }
+    ]
+},{timestamps: true})
 
 export const Doctor = mangoose.model("Doctor", doctorSchema)
